84169cabda2bf02bc3eec6ad219431efed038482
sihun-lee <c.plma.hun@gmail.com>
2024-02-06 16:12:20 +0900

    2024-02-06

---
+++ 01_statement/04_foreach/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_iam_user" "example" {
+  for_each = toset(var.user_names)
+  name     = each.value
+}
+
+output "all_users" {
+    value = aws_iam_user.example[*]
+}
+
+output "all_users_id" {
+    value = values(aws_iam_user.example)[*].id
+}
